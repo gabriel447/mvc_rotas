5b61a8a2534f32d52a507280191bdba925cb193a
Gabriel447 <Gabriel_447@live.com>
2022-10-03 20:30:21 -0300

    initial

---
+++ Controllers/ContatoController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controllers;
+
+class ContatoController
+{
+    public function executar()
+    {
+        echo '<h1>Pagina Contato!</h1>';
+    }
+}
---
+++ Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Controllers;
+
+class HomeController
+{
+    public function executar()
+    {
+        echo '<h1>Estou na Home<h1/>';
+    }
+}
